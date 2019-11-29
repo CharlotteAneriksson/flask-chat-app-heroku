d669f7deed3f5646ab4e62578a453faffa871f9c
Richarlotte <charlotteadolfssonolander@gmail.com>
2019-11-29 14:13:53 +0100

    working on deployment

--- app.py
+++ app.py
@@ -39,5 +39,6 @@ def user(username):
     return render_template("chat.html", username=username, chat_messages=messages)
 
 
-app.run(host=os.getenv("IP", "0.0.0.0"),
-        port=int(os.getenv("PORT", "5000")), debug=False)
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'),
+            port=int(os.getenv('PORT', '5000')), debug=False)
